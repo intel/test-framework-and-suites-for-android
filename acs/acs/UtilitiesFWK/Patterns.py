d7ecacfe6f629e79aa9f1f91df2a07aa7ef1a24c
wangyang <wangyang.guo@intel.com>
2018-08-17 13:25:51 +0800

    acs: rewrite Singlton class
    Change-Id: I4efb2cd0d2bb7dd3602019a8754d770df71fa6fa

--- acs/acs/UtilitiesFWK/Patterns.py
+++ acs/acs/UtilitiesFWK/Patterns.py
@@ -1,47 +1,55 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 """
-Design pattern implementation
+Copyright (C) 2018 Intel Corporation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions
+and limitations under the License.
+
+SPDX-License-Identifier: Apache-2.0
 """
 
 
 class Singleton(object):
 
     """
-    A non-thread-safe helper class to ease implementing singletons.
-    This should be used as a decorator -- not a metaclass -- to the
-    class that should be a singleton.
+    class decorator for singleton.
 
-    The decorated class can define one `__init__` function that
-    takes only the `self` argument. Other than that, there are
-    no restrictions that apply to the decorated class.
+    Usage:
+    - use `.instance()' method to get singleton instance of the class
+    - directly using `cls()` is forbidden, `TypeError` will raise
 
-    To get the singleton instance, use the `instance` method. Trying
-    to use `__call__` will result in a `TypeError` being raised.
-
-    Limitations: The decorated class cannot be inherited from.
+    Cautions:
+    - instance class only call `__init__(self)` without any other arguments
+    - can not inherit from decorated class
 
     """
 
-    def __init__(self, decorated):
-        self._decorated = decorated
+    def __init__(self, clss):
+        self._cls = clss
+
+    def __call__(self):
+        raise TypeError('Singleton should use \'instance()\' to get instance.')
+
+    def __instancecheck__(self, instance):
+        return isinstance(instance, self._cls)
 
     def instance(self):
         """
-        Returns the singleton instance. Upon its first call, it creates a
-        new instance of the decorated class and calls its `__init__` method.
-        On all subsequent calls, the already created instance is returned.
+        Returns the singleton instance.
         """
         if not hasattr(self, '_instance'):
-            self._instance = self._decorated()
+            self._instance = self._cls()
         return self._instance
-
-    def __call__(self):
-        raise TypeError('Singletons must be accessed through `instance()`.')
-
-    def __instancecheck__(self, inst):
-        return isinstance(inst, self._decorated)
 
 
 class Cancel(object):
