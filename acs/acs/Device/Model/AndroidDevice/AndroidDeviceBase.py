5245acad11844dd16d1ff50eea0cdfd4db3893f5
wangji1 <jinliang.wang@intel.com>
2019-08-06 15:11:28 +0800

    format Intel Apache license header in part of .py files
    Change-Id: I5fbe92b99d72731af4efc9e831f2d5b7ceda8cfa
    Signed-off-by: wangji1 <jinliang.wang@intel.com>

--- acs/acs/Device/Model/AndroidDevice/AndroidDeviceBase.py
+++ acs/acs/Device/Model/AndroidDevice/AndroidDeviceBase.py
@@ -1,28 +1,22 @@
 """
-:copyright: (c)Copyright 2013, Intel Corporation All Rights Reserved.
-The source code contained or described here in and all documents related
-to the source code ("Material") are owned by Intel Corporation or its
-suppliers or licensors. Title to the Material remains with Intel Corporation
-or its suppliers and licensors. The Material contains trade secrets and
-proprietary and confidential information of Intel or its suppliers and
-licensors.
+Copyright (C) 2018 Intel Corporation
 
-The Material is protected by worldwide copyright and trade secret laws and
-treaty provisions. No part of the Material may be used, copied, reproduced,
-modified, published, uploaded, posted, transmitted, distributed, or disclosed
-in any way without Intel's prior express written permission.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-No license under any patent, copyright, trade secret or other intellectual
-property right is granted to or conferred upon you by disclosure or delivery
-of the Materials, either expressly, by implication, inducement, estoppel or
-otherwise. Any license under such intellectual property rights must be express
-and approved by Intel in writing.
+http://www.apache.org/licenses/LICENSE-2.0
 
-:organization: INTEL MCG PSI
-:summary: This file implements the AndroidDeviceBase reference phone for ACS Dev
-:since: 11/10/2011
-:author: jreynaud
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions
+and limitations under the License.
+
+SPDX-License-Identifier: Apache-2.0
 """
+
+# flake8: noqa: W504
 import imp
 import os
 import re
@@ -885,7 +879,7 @@ class AndroidDeviceBase(DeviceBase):
         try:
             if status == Global.SUCCESS:
                 splitted_output = status_msg.splitlines()
-                key_regex = ".*\[(?P<key>(.*))\]: \[(?P<value>(.*))\].*"
+                key_regex = ".*\\[(?P<key>(.*))\\]: \\[(?P<value>(.*))\\].*"
 
                 # for each string (describing a property name and value),
                 # we will retrieve name and value for store it into a dictionary
@@ -2739,7 +2733,7 @@ class AndroidDeviceBase(DeviceBase):
 
             if status == Global.SUCCESS:
                 # retrieve the first serial number
-                expr = "(?P<serial>[0-9A-Za-z\-]*).*"
+                expr = "(?P<serial>[0-9A-Za-z\\-]*).*"
                 result = re.match(expr, status_msg)
                 # if the result of math method isn't None,
                 # it means that we find a serial number
